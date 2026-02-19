685884aad51c785ee6c3e6712369769ba9d78cda
Abdulla Rahman <abdulla30r@gmail.com>
2026-02-19 17:53:00 +0600

    postgres connected

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import pool from './db.js';
+dotenv.config();
 const app = express();
-const port = 3000;
-app.listen(port, () => {
+const port = process.env.PORT;
+app.listen(port, async () => {
     console.log(`Listening on port ${port}`);
+    try {
+        const res = await pool.query('SELECT NOW()'); // query returns current DB time
+        console.log('Database connected');
+        console.log('Current time:', res.rows[0].now); // print the time
+    }
+    catch (err) {
+        console.error('Database connection failed:', err);
+    }
 });
 //# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import pool from './db.js'
+
+dotenv.config();
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
 
-app.listen(port, ()=> {
+app.listen(port, async ()=> {
     console.log(`Listening on port ${port}`);
+    try {
+        const res = await pool.query('SELECT NOW()'); // query returns current DB time
+        console.log('Database connected at:', res.rows[0].now); 
+    } catch (err) {
+        console.error('Database connection failed:', err);
+    }
 });
+
